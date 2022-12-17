fd103e6ad10f42435b3f8d455ccd44c0ddbde73a
Lucas Ma <51366352+lucasma8795@users.noreply.github.com>
2022-12-17 16:12:50 +0800

    figured how to dijkstra 👍

--- 12/part1.py
+++ 12/part1.py
@@ -1,7 +1,6 @@
-from dataclasses import dataclass, field
-from itertools import product
+from heapq import *
 from sys import maxsize as INF
-from typing import Optional, Self
+from dataclasses import dataclass
 
 @dataclass
 class Pair:
@@ -10,37 +9,12 @@ class Pair:
 	def __add__(self, other): return Pair(self.x+other.x, self.y+other.y)
 	def out_of_bounds(self):
 		return self.x < 0 or self.x >= ncols or self.y < 0 or self.y >= nrows
-
-@dataclass
-class Node:
-	height: int = 0
-	cost: int = INF
-	prev: Optional[Self] = None
-	seen: bool = False
-	neighbours: list = field(default_factory=list)
-	def __lt__(self, other): return self.cost < other.cost
+	def __hash__(self):
+		return self.y*ncols+self.x
 
 DIR_UP, DIR_DOWN = Pair(-1, 0), Pair(1, 0)
 DIR_LEFT, DIR_RIGHT = Pair(0, -1), Pair(0, 1)
 DIRS = [DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT]
-
-def build_vertices(data):
-	verts = [Node() for _ in range(nrows*ncols)]
-	for r, c in product(range(nrows), range(ncols)):
-		verts[r*ncols+c].height = data[r][c]
-
-	for r, c in product(range(nrows), range(ncols)):
-		u = verts[r*ncols+c]
-		u_pos = Pair(c, r)
-
-		for dir in DIRS:
-			v_pos = u_pos + dir
-			if v_pos.out_of_bounds(): continue
-			v = verts[v_pos.y*ncols+v_pos.x]
-			if v.height - u.height > 1: continue
-			u.neighbours.append(v)
-
-	return verts
 
 data = []
 start, end = None, None
@@ -48,35 +22,56 @@ with open("./data.txt", "r") as fo:
 	for line in fo.readlines():
 		data.append(list(map(ord, line.strip())))
 nrows, ncols = len(data), len(data[0])
+ntotal = nrows*ncols
 
-for r, c in product(range(nrows), range(ncols)):
-	if data[r][c] == ord('S'):
-		data[r][c] = ord('a')
-		start = r*ncols+c
+for r in range(nrows):
+	for c in range(ncols):
+		if data[r][c] == ord('S'):
+			data[r][c] = ord('a')
+			start = r*ncols+c
+		elif data[r][c] == ord('E'):
+			data[r][c] = ord('z')
+			end = r*ncols+c
 
-	elif data[r][c] == ord('E'):
-		data[r][c] = ord('z')
-		end = r*ncols+c
+def build_graph(data):
+	G = [[] for _ in range(ntotal)]
+	for r in range(nrows):
+		for c in range(ncols):
+			u = data[r][c]
+			u_pos = Pair(c, r)
 
-verts = build_vertices(data)
+			for dir in DIRS:
+				v_pos = u_pos + dir
+				if v_pos.out_of_bounds(): continue
+				v = data[v_pos.y][v_pos.x]
+				if v - u > 1: continue
+				G[hash(u_pos)].append(hash(v_pos))
 
-def dijkstra(verts, start, end):
-	q = []
-	verts[start].cost = 0
-	for v in verts: q.append(v)
-	
-	while len(q) > 0:
-		u = q.pop(min(range(len(q)), key=q.__getitem__))
-		u.seen = True
-		for v in u.neighbours:
-			if v.seen: continue
-			alt = u.cost + 1
-			if alt < v.cost:
-				v.cost = alt
-				v.prev = u
+	return G
 
-		if verts[end].prev is not None:
-			return verts[end].cost
+def dijkstra(G, start):
+	dist = [INF for _ in range(ntotal)]
+	prev = [None for _ in range(ntotal)]
+	seen = [False for _ in range(ntotal)]
+	dist[start] = 0
 
-ans = dijkstra(verts, start, end)
+	Q = []
+	for i in range(ntotal):
+		Q.append((dist[i], i))
+	heapify(Q)
+
+	while len(Q):
+		_, u = heappop(Q)
+		if seen[u]: continue
+		seen[u] = True
+		for v in G[u]:
+			alt = dist[u] + 1
+			if alt < dist[v]:
+				dist[v], prev[v] = alt, u
+				heappush(Q, (alt, v))
+	return dist
+
+G = build_graph(data)
+dist = dijkstra(G, start)
+ans = dist[end]
 print(ans)
