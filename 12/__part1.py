1600088ef81919409b97e891b5dda5aaeac25c2b
Lucas Ma <51366352+lucasma8795@users.noreply.github.com>
2022-12-17 11:06:04 +0800

    how to dijkstra???

---
+++ 12/__part1.py
@@ -0,0 +1,103 @@
+import heapq
+import itertools
+from sys import maxsize as INF
+from copy import deepcopy
+from dataclasses import dataclass
+
+class Pos:
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+	
+	def __add__(self, other):
+		return Pos(self.row + other.row, self.col + other.col)
+	
+	def __sub__(self, other):
+		return Pos(self.row - other.row, self.col - other.col)
+	
+	def out_of_bounds(self):
+		return self.row < 0 or self.row >= MAX_ROW or self.col < 0 or self.col >= MAX_COL
+
+
+def get_2d(map, pos): return map[pos.row][pos.col]
+def set_2d(map, pos, value): map[pos.row][pos.col] = value
+
+
+@dataclass
+class NodeCost:
+	cost: int
+	pos: Pos
+	def __lt__(self, other): return self.cost < other.cost
+	def __iter__(self):
+		yield self.cost
+		yield self.pos
+
+
+data = []
+with open("./data.txt", "r") as fo:
+	for line in fo.readlines():
+		data.append(list(map(ord, line.strip())))
+
+
+MAX_ROW, MAX_COL = len(data), len(data[0])
+DIR_UP, DIR_DOWN = Pos(-1, 0), Pos(1, 0)
+DIR_LEFT, DIR_RIGHT = Pos(0, -1), Pos(0, 1)
+START = None
+
+
+for row, col in itertools.product(range(MAX_ROW), range(MAX_COL)):
+	pos = Pos(row, col)
+	if get_2d(data, pos) == ord('S'): START = pos
+	elif get_2d(data, pos) == ord('E'): END = pos
+set_2d(data, START, ord('a'))
+set_2d(data, END, ord('z'))
+
+
+empty = [[False for _ in range(MAX_COL)] for _ in range(MAX_ROW)]
+up, down, left, right = [deepcopy(empty) for _ in range(4)]
+
+def _helper(map, pos, dir):
+	new_pos = pos+dir
+	if new_pos.out_of_bounds(): return False
+	old_elevation, new_elevation = get_2d(map, pos), get_2d(map, new_pos)
+	return new_elevation - old_elevation <= 1
+
+for row, col in itertools.product(range(MAX_ROW), range(MAX_COL)):
+	pos = Pos(row, col)
+	set_2d(left, pos, _helper(data, pos, DIR_LEFT))
+	set_2d(right, pos, _helper(data, pos, DIR_RIGHT))
+	set_2d(up, pos, _helper(data, pos, DIR_UP))
+	set_2d(down, pos, _helper(data, pos, DIR_DOWN))
+
+
+def dijkstra():
+	def _helper(pos, dir):
+		new_pos = pos + dir
+		if new_pos.out_of_bounds(): return False
+		old_cost, new_cost = get_2d(costs, new_pos), node_cost + 1
+		if new_cost < old_cost:
+			heapq.heappush(min_cost, NodeCost(new_cost, new_pos))
+			set_2d(costs, new_pos, new_cost)
+			return True
+		return False
+
+	costs = [[INF for _ in range(MAX_COL)] for _ in range(MAX_ROW)]
+	visited = set()
+	set_2d(costs, START, 0)
+
+	min_cost = [NodeCost(0, START)]
+	while len(min_cost) != 0:
+		node_cost, node_pos = heapq.heappop(min_cost)
+		if node_pos in visited: continue
+		visited.add(node_pos)
+
+		if get_2d(up, node_pos): _helper(node_pos, DIR_UP)
+		if get_2d(down, node_pos): _helper(node_pos, DIR_DOWN)
+		if get_2d(left, node_pos): _helper(node_pos, DIR_LEFT)
+		if get_2d(right, node_pos): _helper(node_pos, DIR_RIGHT)
+
+	return costs
+
+costs = dijkstra()
+ans = get_2d(costs, END)
+print(ans)
--- 12/part2.py
+++ 12/part2.py
@@ -1,111 +1,86 @@
-import heapq
-import itertools
+from dataclasses import dataclass, field
+from itertools import product
 from sys import maxsize as INF
-from copy import deepcopy
-from dataclasses import dataclass
-
-class Pos:
-	def __init__(self, row, col):
-		self.row = row
-		self.col = col
-	
-	def __add__(self, other):
-		return Pos(self.row + other.row, self.col + other.col)
-	
-	def __sub__(self, other):
-		return Pos(self.row - other.row, self.col - other.col)
-	
-	def out_of_bounds(self):
-		return self.row < 0 or self.row >= MAX_ROW or self.col < 0 or self.col >= MAX_COL
-
-
-def get_2d(map, pos): return map[pos.row][pos.col]
-def set_2d(map, pos, value): map[pos.row][pos.col] = value
-
+from typing import Optional, Self
 
 @dataclass
-class NodeCost:
-	cost: int
-	pos: Pos
-	def __lt__(self, other): return self.cost < other.cost
-	def __iter__(self):
-		yield self.cost
-		yield self.pos
+class Pair:
+	x: int
+	y: int
+	def __add__(self, other): return Pair(self.x+other.x, self.y+other.y)
+	def out_of_bounds(self):
+		return self.x < 0 or self.x >= ncols or self.y < 0 or self.y >= nrows
 
+@dataclass
+class Node:
+	height: int = 0
+	cost: int = INF
+	prev: Optional[Self] = None
+	seen: bool = False
+	neighbours: list = field(default_factory=list)
+	def __lt__(self, other): return self.cost < other.cost
+
+DIR_UP, DIR_DOWN = Pair(-1, 0), Pair(1, 0)
+DIR_LEFT, DIR_RIGHT = Pair(0, -1), Pair(0, 1)
+DIRS = [DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT]
+
+def build_vertices(data):
+	verts = [Node() for _ in range(nrows*ncols)]
+	for r, c in product(range(nrows), range(ncols)):
+		verts[r*ncols+c].height = data[r][c]
+
+	for r, c in product(range(nrows), range(ncols)):
+		u = verts[r*ncols+c]
+		u_pos = Pair(c, r)
+
+		for dir in DIRS:
+			v_pos = u_pos + dir
+			if v_pos.out_of_bounds(): continue
+			v = verts[v_pos.y*ncols+v_pos.x]
+			if v.height - u.height < -1: continue
+			u.neighbours.append(v)
+
+	return verts
 
 data = []
+start, ends = None, []
 with open("./data.txt", "r") as fo:
 	for line in fo.readlines():
 		data.append(list(map(ord, line.strip())))
+nrows, ncols = len(data), len(data[0])
 
+for r, c in product(range(nrows), range(ncols)):
+	if data[r][c] == ord('S'):
+		data[r][c] = ord('a')
+		ends.append(r*ncols+c)
 
-MAX_ROW, MAX_COL = len(data), len(data[0])
-DIR_UP, DIR_DOWN = Pos(-1, 0), Pos(1, 0)
-DIR_LEFT, DIR_RIGHT = Pos(0, -1), Pos(0, 1)
-START = None
+	elif data[r][c] == ord('E'):
+		data[r][c] = ord('z')
+		start = r*ncols+c
 
-end_list = []
-for row, col in itertools.product(range(MAX_ROW), range(MAX_COL)):
-	pos = Pos(row, col)
-	if get_2d(data, pos) == ord('E'):
-		set_2d(data, pos, ord('z'))
-		START = pos
-		
-	elif get_2d(data, pos) == ord('S'):
-		set_2d(data, pos, ord('a'))
-		end_list.append(pos)
+	elif data[r][c] == ord('a'):
+		ends.append(r*ncols+c)
 
-	elif get_2d(data, pos) == ord('a'):
-		end_list.append(pos)
+verts = build_vertices(data)
 
+def dijkstra(verts, start):
+	q = []
+	verts[start].cost = 0
+	for v in verts: q.append(v)
+	
+	while len(q) > 0:
+		u = q.pop(min(range(len(q)), key=q.__getitem__))
+		u.seen = True
+		for v in u.neighbours:
+			if v.seen: continue
+			alt = u.cost + 1
+			if alt < v.cost:
+				v.cost = alt
+				v.prev = u
+	return verts
 
-empty = [[False for _ in range(MAX_COL)] for _ in range(MAX_ROW)]
-up, down, left, right = [deepcopy(empty) for _ in range(4)]
-
-def _helper(map, pos, dir):
-	new_pos = pos+dir
-	if new_pos.out_of_bounds(): return False
-	old_elevation, new_elevation = get_2d(map, pos), get_2d(map, new_pos)
-	return new_elevation - old_elevation >= -1
-
-for row, col in itertools.product(range(MAX_ROW), range(MAX_COL)):
-	pos = Pos(row, col)
-	set_2d(left, pos, _helper(data, pos, DIR_LEFT))
-	set_2d(right, pos, _helper(data, pos, DIR_RIGHT))
-	set_2d(up, pos, _helper(data, pos, DIR_UP))
-	set_2d(down, pos, _helper(data, pos, DIR_DOWN))
-
-
-def dijkstra():
-	def _helper(pos, dir):
-		new_pos = pos + dir
-		if new_pos.out_of_bounds(): return False
-		old_cost, new_cost = get_2d(costs, new_pos), node_cost + 1
-		if new_cost < old_cost:
-			heapq.heappush(min_cost, NodeCost(new_cost, new_pos))
-			set_2d(costs, new_pos, new_cost)
-			return True
-		return False
-
-	costs = [[INF for _ in range(MAX_COL)] for _ in range(MAX_ROW)]
-	visited = set()
-	set_2d(costs, START, 0)
-
-	min_cost = [NodeCost(0, START)]
-	while len(min_cost) != 0:
-		node_cost, node_pos = heapq.heappop(min_cost)
-		if node_pos in visited: continue
-		visited.add(node_pos)
-
-		if get_2d(up, node_pos): _helper(node_pos, DIR_UP)
-		if get_2d(down, node_pos): _helper(node_pos, DIR_DOWN)
-		if get_2d(left, node_pos): _helper(node_pos, DIR_LEFT)
-		if get_2d(right, node_pos): _helper(node_pos, DIR_RIGHT)
-
-	return costs
-
-costs = dijkstra()
+verts = dijkstra(verts, start)
 ans = INF
-for pos in end_list:
-	ans = min(ans, get_2d(costs, pos))
+for end in ends:
+	ans = min(ans, verts[end].cost)
 print(ans)
